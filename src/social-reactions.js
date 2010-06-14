3344ed9055e5113916f8279966f5ba09a89de198
Artyom Semyonov <sevenov@gmail.com>
2010-06-14 22:45:19 +0400

    first commit

---
+++ src/social-reactions.js
@@ -0,0 +1,142 @@
+
+var Utils = {
+    queryString: function(hash){
+        var outlist = [];
+        for(var k in hash){
+            outlist.push(k + '=' + hash[k]);
+        }
+        return '?' + outlist.join('&');
+    },
+    each: function(list, fn, obj){
+        for(var i=0; i < list.length; ++i){
+            fn.apply(obj, [list[i]]);
+        }
+    }
+};
+
+var Wrapper = {};
+
+Wrapper.jQuery = function(){
+    return this;
+};
+Wrapper.jQuery.prototype = {
+    request: function(options){
+        $.getJSON(
+            options.url + Utils.queryString(options.data) + '&callback=?',
+            options.oncomplete
+        );
+    }
+};
+
+Wrapper.Mootools = function(){
+    return this;
+};
+Wrapper.Mootools.prototype = {
+    request: function(options){
+        new Request.JSONP({
+            url: options.url,
+            data: options.data,
+            oncomplete: options.oncomplete
+        }).send()
+    }
+};
+
+var Service = {};
+
+Service.Twitter = function(url){
+    return this.init(url);
+};
+Service.Twitter.prototype = {
+    name: 'Twitter',
+    apiurl: 'http://backtweets.com/search.json',
+    options: {},
+
+    init: function(url){
+        this.url = url;
+    },
+
+    get data(){
+        return {
+            q: this.url,
+            key: '7673bf05dbfab4158afe'
+        }
+    },
+
+    parseResponse: function(data){
+        return data;
+    }
+};
+
+Service.FriendFeed = function(url){
+    return this.init(url);
+};
+Service.FriendFeed.prototype = {
+    name: 'FriendFeed',
+    apiurl: 'http://friendfeed-api.com/v2/url',
+
+    init: function(url){
+        this.url = url;
+    },
+
+    get data(){
+        return {
+            url: this.url
+        }
+    },
+
+    parseResponse: function(data){
+        return data.entries;
+    }
+};
+
+var Reactions = function(options){
+    return this.init(options);
+};
+
+Reactions.prototype = {
+    events: {},
+    services: [],
+
+    init: function(options) {
+        if(options.wrapper == undefined ||
+           options.services == undefined ){
+            return;
+        }
+
+        this.wrapper = new options.wrapper();
+
+        Utils.each(options.services, function(service){
+            this.services.push(
+                new service(options.url)
+            );
+        }, this);
+
+        return this;
+    },
+
+    fetch: function() {
+        Utils.each(this.services, function(service){
+            var self = this;
+            var oncomplete = function(data) {
+                self.fire('complete', [
+                    service.parseResponse(data),
+                    service.name
+                ]);
+            };
+            this.wrapper.request({
+                url: service.apiurl,
+                data: service.data,
+                oncomplete: oncomplete
+            });
+        }, this);
+    },
+
+    bind: function(name, fn){
+        this.events[name] = fn;
+    },
+
+    fire: function(name, args){
+        this.events[name].apply(this, args);
+    }
+};
+
